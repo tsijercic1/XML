e8c8c19b5b186bf2128be7c94bd916174d46469f
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-11-01 12:07:23 +0100

    XML v1.0-SNAPSHOT

---
+++ src/main/java/com/tsijercic1/common/InvalidXMLException.java
@@ -0,0 +1,11 @@
+package com.tsijercic1.xml.common;
+
+public class InvalidXMLException extends Exception {
+    public InvalidXMLException() {
+        super();
+    }
+
+    public InvalidXMLException(String message) {
+        super(message);
+    }
+}
